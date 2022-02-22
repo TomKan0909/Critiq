61cd726549b3e4a56350758a8a8380bc8edd30f3
TomKan <tomkan0909@gmail.com>
2022-02-21 22:18:51 -0500

    react routes boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Profile from './routes/profile';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
+
+function Home() {
+  return (
+    <>
+      <main>
+        <p>
+          NYU GIRLS ROAST TECH GUYS 
+        </p>
+      </main>
+      <nav>
+        <Link to="/profile">profile</Link>
+      </nav>
+    </>
+  );
+}
+
 
 export default App;
---
+++ frontend/src/routes/profile.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const Profile = () => {
+    return (
+      <>
+        <main>
+          <p>
+            profile
+          </p>
+        </main>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+      </>
+    );
+}
+
+export default Profile;
