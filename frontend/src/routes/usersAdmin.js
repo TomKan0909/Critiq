be96b89ed3aa845e15a4f0fdb8abfe4d4209dbcc
richa.ren@mail.utoronto.ca <richa.ren@mail.utoronto.ca>
2022-02-23 14:37:25 -0500

    reorganize usersAdmin header

--- frontend/src/routes/usersAdmin.js
+++ frontend/src/routes/usersAdmin.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Link } from "react-router-dom";
-import Plot from "../components/usersAdmin/plot";
-import Header from "../components/usersAdmin/header";
+import Plot from "../components/usersAdmin/header/plot";
+import Header from "../components/usersAdmin/header/keyStats";
 
 const UsersAdmin = () => {
 
