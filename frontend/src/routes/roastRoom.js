439241c45fea4dc405afd63e40a2331b4bebd951
richa.ren@mail.utoronto.ca <richa.ren@mail.utoronto.ca>
2022-02-27 23:11:43 -0500

    add profile to roastRoom

--- frontend/src/routes/roastRoom.js
+++ frontend/src/routes/roastRoom.js
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
+import Image from '../components/profile/image';
+import TextCard from "../components/profile/textCard";
+import StatsCard from '../components/profile/stats';
+import Grid from "@mui/material/Grid";
 
 const RoastRoom = () => {
   return (
     <>
       <main>
-        <p>
-          roastRoom
-        </p>
+          <Grid container rowSpacing={1} 
+              columnSpacing={{ xs: 1, sm: 2, md: 3 }} 
+              alignItems="center">
+              <Grid item xs={3}>
+                <Image/>
+                <TextCard/>
+                <StatsCard/>
+              </Grid>
+              <Grid item xs={2}>
+              </Grid>
+          </Grid>
       </main>
       <nav>
         <Link to="/">Home</Link>
