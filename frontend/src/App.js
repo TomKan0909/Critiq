7146387a3788557ec349543cefb74f5169763e8f
jasonjjl1999 <jasonjjl1999@gmail.com>
2022-03-01 01:09:09 -0500

    Add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
+import Login from './routes/login';
 import Profile from './routes/profile';
 import RoastRoom from './routes/roastRoom';
 import UsersAdmin from './routes/usersAdmin';
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
         <Route path="profile" element={<Profile />} />
         <Route path="roastRoom" element={<RoastRoom />} />
         <Route path="usersAdmin" element={<UsersAdmin />} />
@@ -31,6 +33,7 @@ function Home() {
         </p>
       </main>
       <nav>
+        <Link className="navBarLink" to="/login">login</Link>
         <Link className="navBarLink" to="/profile">profile</Link>
         <Link className="navBarLink" to="/roastRoom">roastRoom</Link>
         <Link className="navBarLink" to="/usersAdmin">usersAdmin</Link>
