4c1976bdbab857854b1c637e9c790c8ac93d450a
richa.ren@mail.utoronto.ca <richa.ren@mail.utoronto.ca>
2022-03-01 12:30:06 -0500

    add text bubbles

--- frontend/src/components/roastRoom/message.js
+++ frontend/src/components/roastRoom/message.js
@@ -5,45 +5,55 @@ const Message = (props) => {
 
     // https://stackoverflow.com/a/17756714
     // https://stackoverflow.com/a/61526114
-    const messageStyle = {
+    const viewerMessageStyle = {
         overflowX : 'hidden',
-        overflowWrap: 'break-word'
+        overflowWrap: 'break-word',
+        borderRadius: 10,
+        padding: 1.2,
+    }
+
+    const senderMessageStyle = {
+        overflowX : 'hidden',
+        overflowWrap: 'break-word',
+        borderRadius: 10,
+        padding: 1.2,
     }
 
     const {sender, viewer, content} = props 
 
-    let senderColContent;
-    let nonSenderColContent;
+    let senderColContent = '';
+    let nonSenderColContent = '';
 
     
     if (sender === viewer) {
         senderColContent = content
+        viewerMessageStyle['backgroundColor'] = 'pink'
     } else {
         nonSenderColContent = content
+        senderMessageStyle['backgroundColor'] = '#FBDB65'
     }  
 
     return (
         <Grid container>
             <Grid item xs={4}>
                 <Typography 
-                    align='left' 
                     variant='body1'
                     marginLeft={3}
                     marginY={1} 
                     fontSize={'1.2rem'}
-                    flexWrap='wrap'>
+                    sx={senderMessageStyle}
+                    zeroMinWidth>
                         {nonSenderColContent}
                 </Typography> 
             </Grid>
             <Grid item xs={4}></Grid>
             <Grid item xs={4}>
                 <Typography 
-                    align='right' 
                     variant='body1' 
                     marginRight={3} 
                     marginY={1} 
                     fontSize={'1.2rem'}
-                    sx={messageStyle}
+                    sx={viewerMessageStyle}
                     zeroMinWidth> 
                         {senderColContent}
                 </Typography> 
