685c32fdb4504692c316a68c3e55d80b39525f51
richa.ren@mail.utoronto.ca <richa.ren@mail.utoronto.ca>
2022-03-01 10:57:04 -0500

    add scrollable chat window

--- frontend/src/components/roastRoom/messageLog.js
+++ frontend/src/components/roastRoom/messageLog.js
@@ -2,10 +2,12 @@ import * as React from 'react';
 import { Box } from '@mui/material';
 import Message from './message';
 const MessageList = (props) => {
-
     const {messages, viewer} = props
+    const chatStyle = {
+        overflowY: 'scroll'
+    }
     return (
-        <Box>
+        <Box sx={chatStyle} maxHeight={'80%'}>
             {messages.map(message => (
             <Message 
                 sender={message.sender}
