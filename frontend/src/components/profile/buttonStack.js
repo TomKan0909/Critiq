4a73f76c3420aa33135d1490065484cfbdd907c7
TomKan <tomkan0909@gmail.com>
2022-02-28 20:40:43 -0500

    Rough copy of edit profile
    Rough copy of edit profile

--- frontend/src/components/profile/buttonStack.js
+++ frontend/src/components/profile/buttonStack.js
@@ -1,8 +1,13 @@
 import { Stack, Button, Alert, Snackbar } from "@mui/material";
 import React from 'react';
+import EditProfile from "./editProfile";
 
 export default function ButtonStack () {
     const [open, setOpen] = React.useState(false);
+    const [openEditProfile, setOpenEditProfile] = React.useState(false);
+    const handleOpenEditProfile = () => setOpenEditProfile(true);
+    const handleCloseEditProfile = () => setOpenEditProfile(false);
+
 
     const handleClick = () => {
         setOpen(true);
@@ -16,12 +21,13 @@ export default function ButtonStack () {
         setOpen(false);
       };
 
-
+    console.log(openEditProfile)
     return (
         <Stack spacing='40px' sx={{alignItems: 'center', marginTop: '300px'}}>
-            <Button variant='contained' color='primary' size="medium" sx={{maxWidth:"200px"}}>
+            <Button onClick={handleOpenEditProfile} variant='contained' color='primary' size="medium" sx={{maxWidth:"200px"}}>
                 Edit Profile
             </Button>
+            <EditProfile open={openEditProfile} handleClose={handleCloseEditProfile}/>
             <Button variant='contained' color='success' size="medium" onClick={handleClick} sx={{maxWidth:"200px"}}>
                 Go Live
             </Button>
---
+++ frontend/src/components/profile/editProfile.js
@@ -0,0 +1,193 @@
+import {
+  Modal,
+  Stack,
+  ImageList,
+  ImageListItem,
+  Box,
+  TextField,
+  MenuItem,
+  Container,
+  Select,
+  InputAdornment,
+  InputLabel,
+} from "@mui/material";
+import Image from "./image";
+import React from "react";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+function ImageComponent() {
+  return (
+    <ImageList cols={3}>
+      <ImageListItem>
+        <Image />
+      </ImageListItem>
+      <ImageListItem>
+        <Image />
+      </ImageListItem>
+      <ImageListItem>
+        <Image />
+      </ImageListItem>
+      <ImageListItem>
+        <Image />
+      </ImageListItem>
+      <ImageListItem>
+        <Image />
+      </ImageListItem>
+      <ImageListItem>
+        <Image />
+      </ImageListItem>
+    </ImageList>
+  );
+}
+
+function Prompt() {
+  const [prompt, setPrompt] = React.useState("");
+
+  const handleChange = (event) => {
+    setPrompt(event.target.value);
+  };
+
+  const prompts = [
+    { value: 1, prompt: "Best travel story" },
+    { value: 2, prompt: "A life goal of mine" },
+    { value: 3, prompt: "I take pride in" },
+    { value: 4, prompt: "Biggest risk I've taken" },
+  ];
+
+  return (
+    <Container size="sm">
+      <form component="form">
+        <TextField
+          label="Select Prompt"
+          value={prompt}
+          select
+          onChange={handleChange}
+          variant="filled"
+          sx={{ display: "block", margin: "auto"}}
+        >
+          {prompts.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.prompt}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          id="filled-multiline-static"
+          label="Prompt Response"
+          multiline
+          Prompt
+          Response
+          rows={5}
+          defaultValue="Enter your Prompt Response"
+          variant="filled"
+          sx={{ display: "block", margin: "auto" }}
+        />
+      </form>
+    </Container>
+  );
+}
+
+function InputForm() {
+  const [gender, setGender] = React.useState("");
+  const [alcohol, setAlcohol] = React.useState("");
+
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
+  };
+
+  const handleAlcoholChange = (event) => {
+    setAlcohol(event.target.value);
+  };
+
+  return (
+    <Container size="sm">
+      <form component="form">
+        <TextField
+          label="Gender"
+          value={gender}
+          onChange={handleGenderChange}
+          select
+          sx={{ display: "block", margin: "auto" }}
+        >
+          {["Male", "Female", "Other"].map((gender, index) => (
+            <MenuItem key={index} value={gender}>
+              {gender}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          label="Age"
+          defaultValue="21"
+          sx={{ display: "block", margin: "auto" }}
+        />
+        <TextField
+          label="Height"
+          defaultValue="250"
+          sx={{ display: "block", margin: "auto" }}
+          InputProps={{
+            endAdornment: <InputAdornment position="end">cm</InputAdornment>,
+          }}
+        />
+        <TextField
+          label="Location"
+          defaultValue="Vatican City"
+          sx={{ display: "block", margin: "auto" }}
+        />
+        <TextField
+          label="Ethnicity"
+          defaultValue="Alien"
+          sx={{ display: "block", margin: "auto" }}
+        />
+        <TextField
+          label="Alcohol"
+          value={alcohol}
+          select
+          onChange={handleAlcoholChange}
+          sx={{ display: "block", margin: "auto" }}
+        >
+          {["Yes", "No", "Sometimes"].map((pref, index) => (
+            <MenuItem key={index} value={pref}>
+              {pref}
+            </MenuItem>
+          ))}
+        </TextField>
+      </form>
+    </Container>
+  );
+}
+
+// TO IMPLEMENT
+// Image List
+// Cards that allow to edit prompts - wrap previously made cards into modal when clicked should open up input form
+// Form to enter Age (Enter a number), Gender (Dropdown M/F/T), Height (string input ()),
+// Location (string input), Ethnicity (string input), Alcohol preference (Dropdown Yes/no)
+
+export default function EditProfile({ open, handleClose }) {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      BackdropProps={{ style: { backgroundColor: "rgba(255, 255, 255, 0.8)" } }}
+      sx={{ overflow: "scroll" }}
+    >
+      <Stack>
+        <ImageComponent />
+        <Prompt />
+        <Prompt />
+        <Prompt />
+        <InputForm />
+      </Stack>
+    </Modal>
+  );
+}
