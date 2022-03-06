834859817d9f7ba142fab187d341b0e1517691c0
richa.ren@mail.utoronto.ca <richa.ren@mail.utoronto.ca>
2022-03-05 22:24:16 -0500

    fix image

--- frontend/src/data/exampleUser.js
+++ frontend/src/data/exampleUser.js
@@ -1,11 +1,11 @@
 const exampleUser = {
   images: [
     { img: "/images/profiles/10175150_1486212614940481_1072685643_a.jpg" },
-    { img: "/images/profiles/10175149_1426343840949654_359612449_a.jpg" },
     { img: "/images/profiles/10175140_307762902709460_258645496_a.jpg" },
     { img: "/images/profiles/10175326_298963150255019_569923105_a.jpg" },
     { img: "/images/profiles/10245906_236779363176309_1714093208_a.jpg" },
     { img: "/images/profiles/10245925_423275357808082_1989745293_a.jpg" },
+    { img: "/images/profiles/10175149_1426343840949654_359612449_a.jpg" },
   ],
   prompts: [
     { title: "Private Equity", content: "Emily" },
