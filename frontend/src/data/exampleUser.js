7fca4093f6d1dcdb4154bdb01f8dd9b75c9a65d9
richa.ren@mail.utoronto.ca <richa.ren@mail.utoronto.ca>
2022-03-05 22:24:00 -0500

    fix image

--- frontend/src/data/exampleUser.js
+++ frontend/src/data/exampleUser.js
@@ -1,7 +1,7 @@
 const exampleUser = {
   images: [
-    { img: "/images/profiles/10175149_1426343840949654_359612449_a.jpg" },
     { img: "/images/profiles/10175150_1486212614940481_1072685643_a.jpg" },
+    { img: "/images/profiles/10175149_1426343840949654_359612449_a.jpg" },
     { img: "/images/profiles/10175140_307762902709460_258645496_a.jpg" },
     { img: "/images/profiles/10175326_298963150255019_569923105_a.jpg" },
     { img: "/images/profiles/10245906_236779363176309_1714093208_a.jpg" },
