bf6938d52bd86ce0e3defd2d3e17185698f51d9a
TomKan <tomkan0909@gmail.com>
2022-03-06 18:49:55 -0500

    prettier lint

--- frontend/src/components/usersAdmin/keyStats/dataSelector.js
+++ frontend/src/components/usersAdmin/keyStats/dataSelector.js
@@ -1,33 +1,31 @@
-import * as React from 'react';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 const DataSelector = (props) => {
+  const { source, onChange } = props;
 
-    const {source, onChange} = props
+  const handleChange = (event) => {
+    onChange(event);
+  };
 
-    const handleChange = (event) => {
-        onChange(event)
-    };
+  const formTheme = {
+    marginTop: "2em",
+    marginBottom: "2em",
+  };
 
-
-    const formTheme = {
-        marginTop: '2em',
-        marginBottom: '2em'
-    }
-
-    return (
-        <FormControl fullWidth sx={formTheme}>
-            <InputLabel>Data</InputLabel>
-            <Select value={source} label="Age" onChange={handleChange}>
-                <MenuItem value={'None'}>None</MenuItem>
-                <MenuItem value={'ageDistribution'}>Age Distribution</MenuItem>
-                <MenuItem value={'exercise'}>Exercise</MenuItem>
-            </Select>
-        </FormControl> 
-    );
+  return (
+    <FormControl fullWidth sx={formTheme}>
+      <InputLabel>Data</InputLabel>
+      <Select value={source} label="Age" onChange={handleChange}>
+        <MenuItem value={"None"}>None</MenuItem>
+        <MenuItem value={"ageDistribution"}>Age Distribution</MenuItem>
+        <MenuItem value={"exercise"}>Exercise</MenuItem>
+      </Select>
+    </FormControl>
+  );
 };
 
 export default DataSelector;
--- frontend/src/components/usersAdmin/keyStats/plot.js
+++ frontend/src/components/usersAdmin/keyStats/plot.js
@@ -1,98 +1,104 @@
-import React from 'react';
-import { Chart as ChartJS } from 'chart.js/auto'
-import { Chart }            from 'react-chartjs-2'
-import { Container, Typography } from '@mui/material';
+import React from "react";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Chart } from "react-chartjs-2";
+import { Container, Typography } from "@mui/material";
 
 const labels = {
-	ageDistribution: ['18-23','23-28','28-33','38-43','43-over',],
-	exercise: ['Daily', 'Often', 'Moderate', 'Sometimes', 'Never']
-}
+  ageDistribution: ["18-23", "23-28", "28-33", "38-43", "43-over"],
+  exercise: ["Daily", "Often", "Moderate", "Sometimes", "Never"],
+};
 const distribution = {
-	ageDistribution: [30, 25, 10, 5, 5, 5],
-	exercise: [15, 20, 30, 20, 15]
-}
-
+  ageDistribution: [30, 25, 10, 5, 5, 5],
+  exercise: [15, 20, 30, 20, 15],
+};
 
 const titles = {
-	ageDistribution: 'Age Distribution',
-	exercise: 'Exercise'
-}
+  ageDistribution: "Age Distribution",
+  exercise: "Exercise",
+};
 
 const types = {
-	ageDistribution: 'pie',
-	exercise: 'bar'
-}
+  ageDistribution: "pie",
+  exercise: "bar",
+};
 
 //https://react-chartjs-2.js.org/examples/pie-chart
 const backgroundColor = [
-	'rgba(255, 99, 132, 0.2)',
-	'rgba(54, 162, 235, 0.2)',
-	'rgba(255, 206, 86, 0.2)',
-	'rgba(75, 192, 192, 0.2)',
-	'rgba(153, 102, 255, 0.2)',
-	'rgba(255, 159, 64, 0.2)',
-]
+  "rgba(255, 99, 132, 0.2)",
+  "rgba(54, 162, 235, 0.2)",
+  "rgba(255, 206, 86, 0.2)",
+  "rgba(75, 192, 192, 0.2)",
+  "rgba(153, 102, 255, 0.2)",
+  "rgba(255, 159, 64, 0.2)",
+];
 
 const borderColor = [
-	'rgba(255, 99, 132, 1)',
-	'rgba(54, 162, 235, 1)',
-	'rgba(255, 206, 86, 1)',
-	'rgba(75, 192, 192, 1)',
-	'rgba(153, 102, 255, 1)',
-	'rgba(255, 159, 64, 1)',
-]
+  "rgba(255, 99, 132, 1)",
+  "rgba(54, 162, 235, 1)",
+  "rgba(255, 206, 86, 1)",
+  "rgba(75, 192, 192, 1)",
+  "rgba(153, 102, 255, 1)",
+  "rgba(255, 159, 64, 1)",
+];
 
 const data = {
-	ageDistribution: {
-		labels: labels.ageDistribution,
-		datasets: [{
-			data: distribution.ageDistribution,
-			backgroundColor: backgroundColor,	
-			borderColor: borderColor,
-			borderWidth: 1,
-		}]
-	},
-	exercise: {
-		labels: labels.exercise,
-		datasets: [{
-			data: distribution.exercise,
-			backgroundColor: backgroundColor,	
-			borderColor: borderColor,
-			borderWidth: 1,
-		}]
-	}
-}
-
-const options = {
-	ageDistribution: {
-		aspectRatio: 1.2
-	},
-	exercise: {
-		plugins: {
-			legend: {
-			  display: false
-			}
-		}
-	}
-}
-
-
-const Plot = (props) => {
-
-	const {source} = props
-
-	if (source !== 'None') {
-		return (
-			<Container>
-				<Typography variant='h3' gutterBottom>{titles[source]}</Typography>
-				<Chart type={types[source]} data={data[source]} options={options[source]} redraw/>
-			</Container>
-		);
-	} else {
-		return null;
-	}
+  ageDistribution: {
+    labels: labels.ageDistribution,
+    datasets: [
+      {
+        data: distribution.ageDistribution,
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+        borderWidth: 1,
+      },
+    ],
+  },
+  exercise: {
+    labels: labels.exercise,
+    datasets: [
+      {
+        data: distribution.exercise,
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+        borderWidth: 1,
+      },
+    ],
+  },
 };
 
+const options = {
+  ageDistribution: {
+    aspectRatio: 1.2,
+  },
+  exercise: {
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+};
 
+const Plot = (props) => {
+  const { source } = props;
+
+  if (source !== "None") {
+    return (
+      <Container>
+        <Typography variant="h3" gutterBottom>
+          {titles[source]}
+        </Typography>
+        <Chart
+          type={types[source]}
+          data={data[source]}
+          options={options[source]}
+          redraw
+        />
+      </Container>
+    );
+  } else {
+    return null;
+  }
+};
 
 export default Plot;
