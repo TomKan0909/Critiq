8f16addc25bf262521f415d75410df43fc25ac94
TomKan <tomkan0909@gmail.com>
2022-03-30 19:14:58 -0400

    create user router to refactor server.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -12,76 +12,85 @@ const { User } = require('../models/user')
 const { mongoChecker, isMongoError } = require("./helpers/mongo_helpers");
 
 /*** User API routes ****************/
-// Set up a POST route to create a user of your web app (*not* a student).
-router.post('/api/users', mongoChecker, async (req, res) => {
-	// log(req.body)
-
-	// Create a new user
-	const user = new User({
-		email: req.body.email,
-		password: req.body.password
-	})
-
-	try {
-		// Save the user
-		const newUser = await user.save()
-		res.send(newUser)
-	} catch (error) {
-		if (isMongoError(error)) { // check for if mongo server suddenly disconnected before this request.
-			res.status(500).send('Internal server error')
-		} else {
-			log(error)
-			res.status(400).send('Bad Request') // bad request for changing the student.
-		}
-	}
-})
-
-//////////////
-
-/*** Login and Logout routes ***/
 // A route to login and create a session
-router.post('/users/login', mongoChecker, async (req, res) => {
-	const email = req.body.email
-    const password = req.body.password
+router.post("/api/users/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-    try {
-    	// Use the static method on the User model to find a user
-	    // by their email and password.
-		const user = await User.findByEmailPassword(email, password);
-		if (!user) {
-            res.redirect('/login');
-        } else {
-            // Add the user's id and email to the session.
-            // We can check later if the session exists to ensure we are logged in.
+    log(username, password);
+    // Use the static method on the User model to find a user
+    // by their email and password
+    User.findByUsernamePassword(username, password)
+        .then(user => {
+            // Add the user's id to the session.
+            // We can check later if this exists to ensure we are logged in.
             req.session.user = user._id;
-            req.session.email = user.email
-            res.redirect('/dashboard');
-        }
-    } catch (error) {
-    	// redirect to login if can't login for any reason
-    	if (isMongoError(error)) { 
-			res.status(500).redirect('/login');
-		} else {
-			log(error)
-			res.status(400).redirect('/login');
-		}
-    }
-
-})
+            req.session.username = user.username; // we will later send the email to the browser when checking if someone is logged in through GET /check-session (we will display it on the frontend dashboard. You could however also just send a boolean flag).
+            log(req.session)
+            res.send({ currentUser: user.username });
+        })
+        .catch(error => {
+            res.status(400).send()
+        });
+});
 
 // A route to logout a user
-router.get('/users/logout', (req, res) => {
-	// Remove the session
-	req.session.destroy((error) => {
-		if (error) {
-			res.status(500).send(error)
-		} else {
-			res.redirect('/')
-		}
-	})
+router.get("/api/users/logout", (req, res) => {
+    // Remove the session
+    req.session.destroy(error => {
+        if (error) {
+            res.status(500).send(error);
+        } else {
+            res.send()
+        }
+    });
+});
+
+// A route to check if a user is logged in on the session
+router.get("/api/users/check-session", (req, res) => {
+    if (env !== 'production' && USE_TEST_USER) { // test user on development environment.
+        req.session.user = TEST_USER_ID;
+        req.session.email = TEST_USER_EMAIL;
+        res.send({ currentUser: TEST_USER_EMAIL })
+        return;
+    }
+
+    if (req.session.user) {
+        res.send({ currentUser: req.session.email });
+    } else {
+        res.status(401).send();
+    }
+});
+
+/*********************************************************/
+
+/*** API Routes below ************************************/
+//  Create a user 
+router.post('/api/users', mongoChecker, async (req, res) => {
+    log(req.body)
+
+    // Create a new user
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    try {
+        // Save the user
+        const newUser = await user.save()
+        res.send(newUser)
+    } catch (error) {
+        if (isMongoError(error)) { // check for if mongo server suddenly disconnected before this request.
+            res.status(500).send('Internal server error')
+        } else {
+            log(error)
+            res.status(400).send('Bad Request') // bad request for changing the student.
+        }
+    }
 })
 
-
+// A route to update user profile
+// router.post('/api/update_users/')
 
 
 // export the router
