911918abca9e54ddac86c590321c56a3e58c86ab
richa.ren@mail.utoronto.ca <richa.ren@mail.utoronto.ca>
2022-04-04 17:33:27 -0400

    correct mongoURI

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose')
 
 /* Connnect to our database */
 // Get the URI of the local database, or the one specified on deployment.
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/StudentAPI'
+// const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/StudentAPI'
+const mongoURI = 'mongodb+srv://username:5ODC2TDNaairj1dN@cluster0.fmq4b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || 'mongodb://localhost:27017/StudentAPI'
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
