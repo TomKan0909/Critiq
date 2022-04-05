ad61eaa0eca39ff2a0eade6411bf1c25aced9d06
jasonjjl1999 <jasonjjl1999@gmail.com>
2022-04-05 16:48:39 -0400

    fix admin user room

--- frontend/src/components/profile/adminProfile.js
+++ frontend/src/components/profile/adminProfile.js
@@ -1,8 +1,10 @@
-import { Box, Button, Snackbar, Alert } from "@mui/material";
+import { Box, Button, Snackbar, Alert, Grid, Slide } from "@mui/material";
 import React from "react";
 import { deleteUserByID } from "../../apis";
 import Profile from "./profile";
 import { grey } from "@mui/material/colors";
+import Image from "../profile/image";
+import TextCard from "../profile/textCard";
 
 const banButtonStyle = {
   minHeight: 80,
@@ -11,13 +13,20 @@ const banButtonStyle = {
   left: "50%",
   width: "80%",
   transform: "translate(-50%, -50%)",
+  marginTop: "100px",
 };
 
-export default function AdminProfile({ ID, name, images, prompts, tags }) {
+export default function AdminProfile({
+  ID,
+  name,
+  images,
+  prompts,
+  tags,
+  occupation,
+  inProp,
+}) {
   const [open, setOpen] = React.useState(false);
   const [userID, setUserID] = React.useState("");
-
-
 
   const handleButtonOnClick = async () => {
     const res = await deleteUserByID(ID);
@@ -32,9 +41,66 @@ export default function AdminProfile({ ID, name, images, prompts, tags }) {
     setOpen(false);
   };
 
+  const profileStyle = {
+    border: "solid " + grey[400],
+    borderRadius: "10px",
+    backgroundColor: grey[200],
+    marginBottom: "2%",
+    maxHeight: "800px",
+    marginTop: "100px",
+    overflowY: "scroll",
+    "&::-webkit-scrollbar": {
+      width: "0.4em",
+      color: "black",
+    },
+    "::-webkit-scrollbar-thumb:vertical": {
+      backgroundColor: grey[600],
+      borderRadius: "20px",
+    },
+    "::-webkit-scrollbar-track": {
+      backgroundColor: grey[400],
+      borderRadius: "20px",
+    },
+  };
+
+  const imageStyle = {
+    marginBottom: "-20px",
+    borderBottomLeftRadius: "0px",
+    borderBottomRightRadius: "0px",
+  };
+
+  const textCardStyle = {
+    borderTopLeftRadius: "0px",
+    borderTopRightRadius: "0px",
+  };
+
   return (
     <Box>
-      <Profile {...{ name, images, prompts, tags }} />
+      <Grid container spacing={2} sx={{ marginTop: "50px" }}>
+        <Slide in={inProp} timeout={300} direction="up">
+          <Grid item xs={12}>
+            <Grid container>
+              <Grid item xs={4} justifyContent="center">
+                <Grid container justifyContent="center">
+                  <Grid item xs={12}>
+                    <Image img={images[0]} sx={imageStyle} />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextCard
+                      title={occupation}
+                      content={name}
+                      sx={textCardStyle}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={8}>
+                <Profile {...{ name, images, prompts, tags }} />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Slide>
+      </Grid>
       <Button
         fullWidth
         variant="contained"
