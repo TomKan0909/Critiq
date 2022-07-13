070df9969416da68baf100a9409bf5a486fc30fb
TomKan <tomkan0909@gmail.com>
2022-07-13 16:30:53 -0700

    remove

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,4 +1,4 @@
-/* This module will hold our connection to 
+/* This module will hold our connection to
    our mongo server through the Mongoose API.
    We will access the connection in our express server. */
 const mongoose = require("mongoose");
@@ -6,8 +6,6 @@ const mongoose = require("mongoose");
 /* Connnect to our database */
 // Get the URI of the local database, or the one specified on deployment.
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/StudentAPI'
-// const mongoURI =
-//   "mongodb+srv://username:5ODC2TDNaairj1dN@cluster0.fmq4b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
